eef9b5345625c62c69ee3769f3975106bf85293d
Vhmdtvmr112 <ahmedeopop@gmail.com>
2025-06-08 22:10:55 +0300

    Update grupo-hidetag.js

--- plugins/grupo-hidetag.js
+++ plugins/grupo-hidetag.js
@@ -45,7 +45,7 @@ await conn.relayMessage(m.chat, {extendedTextMessage:{text: `${masss}\n${htextos
 }
 handler.help = ['hidetag']
 handler.tags = ['grupo']
-handler.command = ['hidetag', 'notificar', 'notify', 'tag']
+handler.command = ['مخفي4', 'مخفي3', 'مخفي2', 'مخفي1']
 handler.group = true
 handler.admin = true
 
