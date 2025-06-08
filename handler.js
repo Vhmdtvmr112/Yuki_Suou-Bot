2a4b8c2b7df8e19dbdd054af166c42c2c2ea2ae5
Vhmdtvmr112 <ahmedeopop@gmail.com>
2025-06-08 21:50:50 +0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -535,16 +535,16 @@ let user2 = m.pushName || 'Anónimo'
 let verifyaleatorio = ['registrar', 'reg', 'verificar', 'verify', 'register'].getRandom()
 
 const msg = {
-rowner: `『✦』El comando *${comando}* solo puede ser usado por los creadores del bot.`, 
-owner: `『✦』El comando *${comando}* solo puede ser usado por los desarrolladores del bot.`, 
-mods: `『✦』El comando *${comando}* solo puede ser usado por los moderadores del bot.`, 
-premium: `『✦』El comando *${comando}* solo puede ser usado por los usuarios premium.`, 
-group: `『✦』El comando *${comando}* solo puede ser usado en grupos.`,
-private: `『✦』El comando *${comando}* solo puede ser usado al chat privado del bot.`,
-admin: `『✦』El comando *${comando}* solo puede ser usado por los administradores del grupo.`, 
-botAdmin: `『✦』Para ejecutar el comando *${comando}* debo ser administrador del grupo.`,
-unreg: `『✦』El comando *${comando}* solo puede ser usado por los usuarios registrado, registrate usando:\n> » #${verifyaleatorio} ${user2}.${edadaleatoria}`,
-restrict: `『✦』Esta caracteristica está desactivada.`
+rowner: `『✦』 *الأمر ${comando} يستعمل فقط في المجموعات .*`, 
+owner: `*『✦』 الأمر ${comando} يستعمل فقط من قبل المطور .*`, 
+mods: `*『✦』 الأمر ${comando} يستعمل فقط من قبل مشرف البوت .*`, 
+premium: `*『✦』 الأمر ${comando} يتعمل فقط اذا كنت مستخدم مميز .*`, 
+group: `*『✦』 الأمر ${comando} يستعمل فقط في المجموعات .*`,
+private: `*『✦』 الأمر ${comando} يمكن استعماله فقط في الدردشة االخاصة بك .*`,
+admin: `*『✦』 الأمر ${comando} يمكن استعماله فقط من قبل ادمن الجروب .*`, 
+botAdmin: `*『✦』 لإستعمال الأمر ${comando} يجب ان تكون مشرفاً في الجروب .*`,
+unreg: `*『✦』 الأمر${comando} يمكنك استعماله اذا سجلت في البوت سجل من خلال* registrado, registrate usando:\n> » #${verifyaleatorio} ${user2}.${edadaleatoria}`,
+restrict: `*『✦』 هذة الخاصية معطلة*`
 }[type];
 if (msg) return m.reply(msg).then(_ => m.react('✖️'))}
 
